c16500e238d3e888e6863cfa109f81c4230fe0f4
serge <blacksg@mail.ru>
2018-03-11 12:53:58 +0300

    коммит

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::group(['prefix' => '/',
                 'namespace' => 'Api',
-                'as' => 'api.'
+
     ],
     function (){
         Route::resource('/', 'ImagesController',
